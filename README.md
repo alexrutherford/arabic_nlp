aeadd9166e1304a079e2bb1f0e9209ce59b05530
Alex Rutherford <alexisadams@gmail.com>
2014-01-03 12:08:42 -0500

    write out sentiments

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Python scripts to read in raw Arabic text, remove diacritics, perform basic normalisation and stemming and then derive sentiment from term frequency. Positive and negative words are supplied and based on Egyptian and Levantine dialects as well as Fus'ha.
+
+get_sentiment.py requires NumPy, otherwise all code in pure Python
--- get_sentiment.py
+++ get_sentiment.py
@@ -7,102 +7,110 @@
 #################
 import csv,re,sys
 import numpy as np
-import matplotlib.pyplot as plt
 
-v=False
-#v=True
-# Flag to print verbosely
 
-try:
-  inFileHandle=open(sys.argv[1],'r')
-  outFile=csv.writer(open(sys.argv[1].partition('.')[0]+'_normalised.txt','w'),delimiter=' ')
-except:
-  print 'NEED FILE AS FIRST ARG'
-  sys.exit(1)
+###########
+def getWordLists():
+###########
 
-if len(sys.argv)>2:
-  if sys.argv[2]=='-v':
-    v=True
-# Set verbose logging
+  posFile=csv.reader(open('pos_words.txt','r'),delimiter='\t')
+  negFile=csv.reader(open('neg_words.txt','r'),delimiter='\t')
+  negFileAdd=csv.reader(open('neg_words_add.txt','r'),delimiter='\t')
+  stopFile=csv.reader(open('stop_words.txt','r'),delimiter='\t')
+  negationFile=csv.reader(open('negation_words.txt','r'),delimiter='\t')
 
-tweets=[t.decode('utf-8') for t in inFileHandle.readlines()]
+  posWords=[]
+  negWords=[]
+  stopWords=[]
+  negationWords=[]
 
-posFile=csv.reader(open('pos_words.txt','r'),delimiter='\t')
-negFile=csv.reader(open('neg_words.txt','r'),delimiter='\t')
-negFileAdd=csv.reader(open('neg_words_add.txt','r'),delimiter='\t')
-stopFile=csv.reader(open('stop_words.txt','r'),delimiter='\t')
-negationFile=csv.reader(open('negation_words.txt','r'),delimiter='\t')
-
-posWords=[]
-negWords=[]
-stopWords=[]
-negationWords=[]
-
-for line in posFile:
-  if len(line)>0:
-    posWords.append(line[0])
-for line in negFile:
-  if len(line)>0:
-    negWords.append(line[0])
-if True:
-  for line in negFileAdd:
+  for line in posFile:
+    if len(line)>0:
+      posWords.append(line[0])
+  for line in negFile:
     if len(line)>0:
       negWords.append(line[0])
-for line in stopFile:
-  if len(line)>0:
-    stopWords.append(line[0])
-for line in negationFile:
-  if len(line)>0:
-    negationWords.append(line[0])
+  if True:
+    for line in negFileAdd:
+      if len(line)>0:
+        negWords.append(line[0])
+  for line in stopFile:
+    if len(line)>0:
+      stopWords.append(line[0])
+  for line in negationFile:
+    if len(line)>0:
+      negationWords.append(line[0])
+
+  posCount=0
+  negCount=0
+  stopCount=0
+  negationCount=0
+
+  posWords=[p.decode('utf-8') for p in posWords]
+  negWords=[p.decode('utf-8') for p in negWords]
+  stopWords=[p.decode('utf-8') for p in stopWords]
+  negationWords=[p.decode('utf-8') for p in negationWords]
+
+  return posWords,negWords,stopWords,negationWords
+
+###########
+def main():
+###########
+
+  try:
+    inFileHandle=open(sys.argv[1],'r')
+  except:
+    print 'NEED FILE AS FIRST ARG'
+    sys.exit(1)
+
+  v=False
+  # Flag to print verbosely
+
+  if len(sys.argv)>2:
+    if sys.argv[2]=='-v':
+      v=True
+# Set verbose logging
+
+  tweets=[t.decode('utf-8') for t in inFileHandle.readlines()]  
+  
+  posWords,negWords,stopWords,negationWords=getWordLists()
 
 ########################
-testTweet=u'انا سعيد جدا بس ما أعرف ليس انت هزين شرموط'
-testTweet=u'مبقاش بياكل الشويتين دول خلاص وفر هم لنفسك'
-#testTweet=u'كمان اسجل اعتراضى كمان محتجه'
-# Negative ?
+  
+  positives=np.zeros(shape=len(tweets))
+  negatives=np.zeros(shape=len(tweets))
 
-#testTweet=u'يارب اكتب الحريه لثوار المحاكمات العسكريه'
-# Positive ?
+  for t,tweet in enumerate(tweets):
 
-posCount=0
-negCount=0
-stopCount=0
-negationCount=0
+    posCount=negCount=stopCount=negationCount=0
 
-posWords=[p.decode('utf-8') for p in posWords]
-negWords=[p.decode('utf-8') for p in negWords]
-stopWords=[p.decode('utf-8') for p in stopWords]
-negationWords=[p.decode('utf-8') for p in negationWords]
+    for w,word in enumerate(tweet.split(' ')):
 
-positives=np.zeros(shape=len(tweets))
-negatives=np.zeros(shape=len(tweets))
+      if v:print word,
 
-for t,tweet in enumerate(tweets):
+      if word in posWords:
+        posCount+=1
+        if v:print ' => POS'
+        continue
+      if word in negWords:
+        negCount+=1
+        if v:print ' => NEG'
+        continue
+      if word in stopWords:
+        stopCount+=1
+        if v:print ' => STOP'
+        continue
+      if word in negationWords:
+        negationCount+=1
+        if v:print ' => NEGATION'
+        continue
+      if v:print ''
+    if v:print '(pos,neg,stop,negation) = ',(posCount,negCount,stopCount,negationCount)
+    positives[t]=posCount
+    negatives[t]=negCount
+  
+  np.savetxt('sentiments.txt',np.vstack((positives,negatives)).T,fmt="%d",delimiter='\t') 
 
-  posCount=negCount=stopCount=negationCount=0
-
-  for w,word in enumerate(tweet.split(' ')):
-
-    if v:print word,
-
-    if word in posWords:
-      posCount+=1
-      if v:print ' => POS'
-      continue
-    if word in negWords:
-      negCount+=1
-      if v:print ' => NEG'
-      continue
-    if word in stopWords:
-      stopCount+=1
-      if v:print ' => STOP'
-      continue
-    if word in negationWords:
-      negationCount+=1
-      if v:print ' => NEGATION'
-      continue
-    if v:print ''
-  if v:print '(pos,neg,stop,negation) = ',(posCount,negCount,stopCount,negationCount)
-  positives[t]=posCount
-  negatives[t]=negCount
+if __name__=="__main__":
+  main()
 
